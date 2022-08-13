baff6ef09be11e5a1064f76109bc2b90f6ff1fde
Saujan Shrestha <99749301+SaujanShr@users.noreply.github.com>
2022-08-13 12:11:38 +0100

    done scraping, now push into sql

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ async function updateDatabase()
     }
 
     // Get a list of hero data.
-    // [pos, heroName, playRate, winRate]
+    // [heroName, position, playRate, winRate]
     let heroData = []
     for(let i = 0; i < heroList.length; i++)
     {
@@ -29,13 +29,27 @@ async function updateDatabase()
         htmlDoc = parser.parseFromString(html, 'text/html');
 
         let heroRoleHeaders = htmlDoc.querySelector('[*|class="content-box-lvl-2 roles"]')
-                                     .getElementsByClassName("content-box-header");
+                                     .getElementsByClassName("content-box-lvl-3");
 
-        for (let i = 0; i < heroRoleHeaders.length; i++)
+        for (let j = 0; j < heroRoleHeaders.length; j++)
         {
-            let role = heroRoleHeaders.querySelector('[*|class="header-role-info"]')
-                                      .textContext;
-            console.log(role);
+            let role = heroRoleHeaders[j].querySelector('[*|class="header-role-info"]')
+                                         .textContent;
+            if (role.includes("Most Played"))
+                role = role.replace("Most Played", "");
+            role = role.trim();
+
+            let htmlRoleStats = heroRoleHeaders[j].querySelector('[*|class="header-role-stats"]')
+                                                  .getElementsByClassName("stat-info");
+            let roleStats = [];
+            roleStats.push(htmlRoleStats[0].textContent);
+            roleStats.push(htmlRoleStats[1].textContent);
+            roleStats[0] = roleStats[0].substring(0, roleStats[0].indexOf("matches")).trim();
+            roleStats[1] = roleStats[1].trim().substring(10).trim();
+
+            heroData.push([heroList[i], role, roleStats[0], roleStats[1]]);
         }
     }
+
+
 }
